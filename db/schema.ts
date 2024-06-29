da30f51c5281628478ac76c9ba7a0757b3984d92
Amrit Rai <iamamrit27@gmail.com>
2024-06-29 19:22:26 +0530

    initial setup

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { text, timestamp, pgTable, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  email: text("email").notNull().unique(),
+  username: text("username").notNull().unique(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
